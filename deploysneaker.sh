2676b62a6dccd7a71d3751887674c6e1dd0c287a
Jim McBride <jm@gonkulator.io>
2015-10-01 12:08:36 -0500

    remove colors and fix status

--- deploysneaker.sh
+++ deploysneaker.sh
@@ -21,34 +21,12 @@ export cnvmipaddr=$4
 export PID=$$
 
 
-##colors
-RESTORE=$(echo '\033[0m')
-RED=$(echo '\033[00;31m')
-GREEN=$(echo '\033[00;32m')
-YELLOW=$(echo '\033[00;33m')
-BLUE=$(echo '\033[00;34m')
-MAGENTA=$(echo '\033[00;35m')
-PURPLE=$(echo '\033[00;35m')
-CYAN=$(echo '\033[00;36m')
-DARKGRAY=$(echo '\033[00;90m')
-LIGHTGRAY=$(echo '\033[00;37m')
-LRED=$(echo '\033[01;31m')
-LGREEN=$(echo '\033[01;32m')
-LYELLOW=$(echo '\033[01;33m')
-LBLUE=$(echo '\033[01;34m')
-LMAGENTA=$(echo '\033[01;35m')
-LPURPLE=$(echo '\033[01;35m')
-LCYAN=$(echo '\033[01;36m')
-WHITE=$(echo  '\033[01;37m')
-##colors
-
-
 
 	status() {
-	echo "${GREEN}█ ${GRAY} $*${RESTORE}"
+	echo "[o] $*"
 }
 	error() {
-		echo "${LRED}█${RED} $*${RESTORE}"
+		echo "[-] ERROR: $*"
 	}
 
 
